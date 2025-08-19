d6d4db139eaa73a089c7ffccde855b91e2dcf259
sachinsingh <sachinsingh6386@gmail.com>
2025-08-19 21:06:54 +0530

    add cookies and jwt

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ const connectDB=require("./config/database.js");
 const User=require("./model/user.js")
 const {validateSignUpData}=require("./utils/validation.js")
 const bcrypt=require("bcrypt")
+const cookieParser=require("cookie-parser")
+const jwt=require("jsonwebtoken");
 const app=express();
 app.use(express.json());
+app.use(cookieParser());
+
 
 //signUP APIs
 app.post("/signup",async(req,res)=>{
@@ -43,6 +47,11 @@ app.post("/login",async(req,res)=>{
     const isPasswordValid=await bcrypt.compare(password,user.password);
     if(isPasswordValid)
     {
+        // Create a JWT token
+        const token=await jwt.sign({_id:user._id},"DEV@Tinder$790");
+
+        //console.log(token);
+        res.cookie("token",token);
         res.send("Login successful !");
     }
     else{
@@ -127,6 +136,36 @@ app.patch("/user/:userId",async(req,res)=>{
        res.status(400).send("Something went wrong"+err.message);  
     }
 });
+
+//Profile APIs
+app.get("/profile",async(req,res)=>{
+    try{
+    const cookies=req.cookies;
+    const {token}=cookies;
+    if(!token)
+    {
+        throw new Error("Invalid Token");
+    }
+    //validate my token
+    const decodedMessage=await jwt.verify(token,"DEV@Tinder$790");
+    console.log(decodedMessage);
+    const {_id}=decodedMessage;
+   
+    const user=await User.findById(_id);  
+    if(!user)
+    {
+        throw new Error("User does not exist")
+    }
+  
+    res.send(user)
+    }catch(err)
+    {
+        res.status(400).send("ERROR :"+err.message); 
+    }
+})
+
+
+
 connectDB()
 .then(()=>{
     console.log("DataBase connection established..");
