c8b15f8f9900effc8fb06fb76db6d27a36954233
sachinsingh <sachinsingh6386@gmail.com>
2025-08-22 18:07:36 +0530

    add Schema

--- src/model/connectionRequest.js
+++ src/model/connectionRequest.js
@@ -14,7 +14,7 @@ const connectionRequestSchema=new mongoose.Schema(
          type:String,
          required:true,
          enum:{
-            values:["ignore","interested","accepeted","rejected"],
+            values:["ignore","interested","accepted","rejected"],
                 message: `{VALUE} is incorrect status type`,  
          },
         },
@@ -23,6 +23,7 @@ const connectionRequestSchema=new mongoose.Schema(
         timestamps:true,
     }
 );
+connectionRequestSchema.index({fromUserId:1,toUserId:1});
 connectionRequestSchema.pre("save",function(next){
     const connectionRequest=this;
     if(connectionRequest.fromUserId.equals(connectionRequest.toUserId))
